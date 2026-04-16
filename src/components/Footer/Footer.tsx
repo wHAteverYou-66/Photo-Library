cfa56ffa04ee13b8efe279b4b72ada91d8d346bc
Nitika Padmakar <148265935+wHAteverYou-66@users.noreply.github.com>
2026-04-16 17:12:04 +0530

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="mt-20">
       <Container className="p-6">
         <p className="text-center text-zinc-500">
-          Built with <a className="underline font-medium text-inherit" href="https://cloudinary.com/">Cloudinary</a> &amp; <a className="underline font-medium text-inherit" href="https://nextjs.org/">Next.js</a> by <a className="underline font-medium text-inherit" href="https://twitter.com/colbyfayock">Nitika Padmakar</a>
+          Built with <a className="underline font-medium text-inherit" href="https://cloudinary.com/">Cloudinary</a> &amp; <a className="underline font-medium text-inherit" href="https://nextjs.org/">Next.js</a> by <a className="underline font-medium text-inherit" href="https://www.linkedin.com/in/nitika-padmakar-862665258">Nitika Padmakar</a>
         </p>
       </Container>
     </footer>
